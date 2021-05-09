18766d0357cb4e4c83481ece77f2815791a5945e
salimthedelegue <61408128+salimthedelegue@users.noreply.github.com>
2021-05-09 15:50:57 +0200

    Add files via upload

---
+++ commande.php
@@ -0,0 +1,45 @@
+<?php
+
+class Commande
+
+{   private ?int $id=null;;
+	private ?string $name ;
+	private ?string $prix;
+	private ?string $email;
+	private ?string $produit;
+	private ?string $adress;
+	private ?int $phone;
+	private ?int $pmode;
+	
+
+	public function __construct($name,$prix,$email,$produit,$adress,$phone,$pmode)
+	{
+		$this->name=$name;
+		$this->prix=$prix;
+        $this->produit=$produit;
+        $this->email=$email;
+		$this->adress=$adress;
+		$this->phone=$phone;
+		$this->pmode=$pmode;
+		
+	}
+	
+	public function getName(){return $this->name;}
+	public function getPrix(){return $this->prix ;}
+	public function getProduit(){return $this->produit;}
+	public function getEmail(){return $this->email;}
+	public function getAdress(){return $this->adress;}
+	public function getPhone(){return $this->phone;}
+	public function getPmode(){return $this->pmode;}
+	
+
+	public function setName($name){$this->name=$name;}
+	public function setPrix($prix){$this->prix=$prix;}
+	public function setProduit($produit){$this->produit=$produit;}
+	public function setEmail($email){$this->email=$email;}
+	public function setAdresse($adress){$this->adress=$adress;}
+	public function setPhone($phone){$this->phone=$phone;}
+	public function setPmode($pmode){$this->pmode=$pmode;}
+	
+}
+?>
---
+++ commandeC.php
@@ -0,0 +1,151 @@
+<?php
+include "configg.php";
+
+class commandeC
+{
+  
+	public function ajouterCommande($commande)
+ {
+        $db=config::getConnexion();
+        $sql="INSERT INTO commande(name,prix,produit,email,adress,phone,pmode) VALUES (:name, :prix, :produit, :email, :adress, :phone,:pmode)";
+       
+        try{
+			
+            $req = $db->prepare($sql);
+			
+            $name=$commande->getName();
+            $prix=$commande->getPrix();
+            $produit=$commande->getProduit();
+            $email=$commande->getEmail();
+            $adress=$commande->getAdress();
+            $phone=$commande->getPhone();
+            $pmode=$commande->getPmode();
+
+			$req->bindValue(':name',$name);
+            $req->bindValue(':prix',$prix);
+            $req->bindValue(':produit',$produit);
+            $req->bindValue(':email',$email);
+            $req->bindValue(':adress',$adress);
+            $req->bindValue(':phone',$phone);
+            $req->bindValue(':pmode',$pmode);
+			$req->execute();
+			
+			
+		} 
+		catch(Exception $e)
+		{
+			die('404:Error');
+
+		}
+	}
+    public function supprimerCommande($id){
+        $db=config::getConnexion();
+        $sql="DELETE FROM commande WHERE id= :id";
+        
+        $req=$db->prepare($sql);
+		
+        $req->bindValue(':id',$id);
+        try{
+            $req->execute();
+			return true ;
+        }
+        catch (Exception $e){
+            die('Erreur: '.$e->getMessage());
+        }
+		
+    }
+
+
+public function modifierCommande($id,$name,$phone,$email,$produit)
+{
+
+	$sql="UPDATE commande SET name= '$name', phone='$phone', produit='$produit', email='$email' WHERE id='$id'";
+	$db = config::getConnexion();
+	try
+	{
+		$db->query($sql);
+	}
+	catch (Exception $e)
+	{
+		die('Erreur: '.$e->getMessage());
+	}
+}
+
+public function modifierCommande2($id,$adress)
+{
+
+	$sql="UPDATE commande SET adress= '$adress' WHERE id='$id'";
+	$db = config::getConnexion();
+	try
+	{
+		$db->query($sql);
+	}
+	catch (Exception $e)
+	{
+		die('Erreur: '.$e->getMessage());
+	}
+}
+
+public function modifierCommande3($id,$pmode)
+{
+
+	$sql="UPDATE commande SET pmode='$pmode' WHERE id='$id'";
+	$db = config::getConnexion();
+	try
+	{
+		$db->query($sql);
+	}
+	catch (Exception $e)
+	{
+		die('Erreur: '.$e->getMessage());
+	}
+}
+function afficherCommande($search)
+    {
+
+        $sql="SELECT * FROM commande WHERE name='$search'";
+
+        $db = config::getConnexion();
+        try
+        {
+            $query=$db->prepare($sql);
+			$query->execute([
+				'login' => $search
+			]);
+            return $query->fetch();
+        }
+        catch (Exception $e)
+        {
+            die('Erreur: '.$e->getMessage());
+        }
+    }
+
+
+
+
+
+	function afficherCommandetri($cc)
+    {
+        
+        $sql="SELECT* FROM commande ORDER BY $cc ASC";
+
+        $db = config::getConnexion();
+        try
+        {
+            $list=$db->query($sql);
+            return $list;
+        }
+        catch (Exception $e)
+        {
+            die('Erreur: '.$e->getMessage());
+        }
+    }
+
+
+
+}
+
+  
+
+
+?>
